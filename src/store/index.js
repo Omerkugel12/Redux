9a218939ce2ed5210dbf05a2ecfe1eb38e7ec47d
Omerkugel12 <omerkugel12@gmail.com>
2024-07-13 21:03:15 +0300

    cars-json

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import counterReducer from "./reducers/counter.reducer";
 import settingsReducer from "./reducers/settings.reducer";
 import catsReducer from "./reducers/cats.reducer";
 import { thunk } from "redux-thunk";
+import { carsReducer } from "./reducers/cars.reducer";
 
 const rootReducer = combineReducers({
   counterModule: counterReducer,
   settingsModule: settingsReducer,
   catsModule: catsReducer,
+  carsModule: carsReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
---
+++ src/store/reducers/cars.reducer.js
@@ -0,0 +1,5 @@
+const INITIAL_CARS_STATE = {
+  cars: [],
+};
+
+export function carsReducer(state = INITIAL_CARS_STATE, action) {}
